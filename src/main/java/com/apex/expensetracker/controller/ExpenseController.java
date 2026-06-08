27ce7830c17312bcc1b22bba7096f2b0526fe625
Hardik Rathore <hardikrathore1009@gmail.com>
2026-06-08 20:00:23 +0530

    Project Initialised

---
+++ src/main/java/com/apex/expensetracker/controller/ExpenseController.java
@@ -0,0 +1,14 @@
+package com.apex.expensetracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Expense Tracker API Running!!!";
+    }
+    
+}
